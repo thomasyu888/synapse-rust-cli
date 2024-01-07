9a2da7fe25bce4d1a6512a456b09a646d4c7b9e4
Thomas Yu <thomas.yu@sagebase.org>
2024-01-07 14:57:37 -0800

    Add in test call to synapse API

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 
+use std::env;
+
 use anyhow::{Context, Result};
 use clap::Parser;
+use reqwest::Error;
 
 // Three / will add to the cli helper
 /// Search for a pattern in a file and display the lines that contain it.
@@ -21,9 +24,27 @@ struct Cli {
 //     }
 // }
 
+#[tokio::main]
+async fn test_execution() -> Result<(), Error> {
+    let token = env::var("SYNAPSE_AUTH_TOKEN").unwrap();
+    // let bearer = "Bearer your_token_here";
+    let client = reqwest::Client::new();
+    let res = client.get("https://repo-prod.prod.sagebase.org/repo/v1/userProfile")
+        .header("Accept", "application/json")
+        .header("User-Agent", "Rust")
+        .bearer_auth(token)
+        .send()
+        .await?;
+
+    let text = res.text().await?;
+    println!("{}", text);
+
+    Ok(())
+}
+
 fn main() -> Result<()> {
     let args = Cli::parse();
-
+    test_execution()?;
     // let content = std::fs::read_to_string(&args.path)?;
     let content = std::fs::read_to_string(&args.path)
         .with_context(|| format!("could not read file `{}`", args.path.display()))?;
