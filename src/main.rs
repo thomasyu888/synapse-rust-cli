393d111934b234900680555fc419883b374be83b
Thomas Yu <thomas.yu@sagebase.org>
2024-01-07 14:22:11 -0800

    Add instructions

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use clap::Parser;
 
+// Three / will add to the cli helper
+/// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
 struct Cli {
+    /// The pattern to look for
     pattern: String,
+    /// The path to the file to read
     path: std::path::PathBuf,
 }
 
